c326751cd2293c919a1e45c36a74c8683adbcdf2
Kingsman1996 <trader96non@gmail.com>
2025-06-13 16:24:58 +0700

    sửa lại question service

--- src/main/java/app/repository/QuestionRepository.java
+++ src/main/java/app/repository/QuestionRepository.java
@@ -2,15 +2,9 @@ package app.repository;
 
 import app.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import java.util.List;
-
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    @Query("SELECT q FROM Question q WHERE q.user.id = :userId")
-    List<Question> findAllByUserId(@Param("userId") long userId);
     long countByCategoryId(Long categoryId);
 
     boolean existsByCategoryId(Long categoryId);

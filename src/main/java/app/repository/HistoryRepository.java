c05ff98ed6f193a81a4c2f8c430a27a6eb215857
Your Namelesonnhat <sonnhatbest@gmail.com>
2025-07-08 17:26:44 +0700

    Nhất quay lại commit History của Đạt

--- src/main/java/app/repository/HistoryRepository.java
+++ src/main/java/app/repository/HistoryRepository.java
@@ -4,8 +4,6 @@ import app.entity.Exam;
 import app.entity.History;
 import app.entity.Room;
 import app.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,9 +11,9 @@ import java.util.List;
 
 @Repository
 public interface HistoryRepository extends JpaRepository<History, Long> {
-    Page<History> findByUserIdOrderByFinishedAtDesc(Long userId, Pageable pageable); // Thay List bằng Page
+    List<History> findByUserIdOrderByFinishedAtDesc(Long userId);
 
-    Page<History> findHistoriesByRoom_HostOrderByFinishedAtDesc(User roomHost, Pageable pageable); // Thay List bằng Page
+    List<History> findHistoriesByRoom_HostOrderByFinishedAtDesc(User roomHost);
 
     List<History> findByRoomOrderByScoreDesc(Room room);
 

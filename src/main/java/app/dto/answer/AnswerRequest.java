534988cb42523aa6ffc3c2ddf465c5e396f43a1b
npv34 <phanvanluan611996@gmail.com>
2025-07-10 13:53:33 +0700

    chỉnh sửa lại cập nhật ảnh câu hỏi trong supabase

--- src/main/java/app/dto/answer/AnswerRequest.java
+++ src/main/java/app/dto/answer/AnswerRequest.java
@@ -2,11 +2,13 @@ package app.dto.answer;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class AnswerRequest {
     private String content;
     private boolean correct;
--- src/main/java/app/service/QuestionService.java
+++ src/main/java/app/service/QuestionService.java
@@ -1,33 +1,23 @@
 package app.service;
 
+import app.dto.answer.AnswerRequest;
 import app.dto.question.*;
 import app.entity.*;
 import app.exception.*;
 import app.repository.*;
 import app.util.MessageHelper;
 import lombok.RequiredArgsConstructor;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.InputStream;
-import java.nio.file.StandardCopyOption;
 import java.util.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.stream.Collectors;
 
 @Service
@@ -55,81 +45,19 @@ public class QuestionService {
     }
 
     @Transactional
-    public void addQuestion(AddQuestionRequest request, MultipartFile image) throws IOException {
-        Question question = new Question();
-        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-        User foundUser = userRepository.findByEmail(userEmail)
-                .orElseThrow(() -> new NotFoundException(messageHelper.get("user.not.found")));
-        question.setUser(foundUser);
-        question.setCategory(categoryRepository.findByName(request.getCategory()));
-        question.setType(typeRepository.findByName(request.getType()));
-        question.setDifficulty(difficultyRepository.findByName(request.getDifficulty()));
-        question.setContent(request.getContent());
-        for (Answer item : request.getAnswers()) {
-            item.setQuestion(question);
-        }
-        question.setAnswers(request.getAnswers());
-
-        if (image != null && !image.isEmpty()) {
-            try {
-                String originalName = StringUtils.cleanPath(Objects.requireNonNull(image.getOriginalFilename()));
-                if (originalName.isBlank()) {
-                    throw new UploadException(messageHelper.get("file.name.invalid"));
-                }
-
-                String contentType = image.getContentType();
-                if (contentType == null || !contentType.startsWith("image/")) {
-                    throw new UploadException(messageHelper.get("image.type.invalid"));
-                }
-                String extension = originalName.substring(originalName.lastIndexOf("."));
-                String baseName = originalName.substring(0, originalName.lastIndexOf(".")).replaceAll("[^a-zA-Z0-9_-]", "");
-                String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
-                String safeFileName = baseName + "_" + timestamp + extension;
-
-                Path uploadPath = Paths.get(uploadDirectory);
-                Files.createDirectories(uploadPath);
-
-                Path destination = uploadPath.resolve(safeFileName);
-                Files.copy(image.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
-                question.setImage(urlPrefix + safeFileName);
-            } catch (IOException e) {
-                throw new UploadException(messageHelper.get("file.upload.error") + ": " + e.getMessage());
-            }
-        }
-        questionRepository.save(question);
-    }
-
-    @Transactional
     public void storeQuestion(QuestionRequest request) {
         Question question = new Question();
+
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
         User foundUser = userRepository.findByEmail(userEmail)
                 .orElseThrow(() -> new NotFoundException(messageHelper.get("user.not.found")));
         question.setUser(foundUser);
-        question.setCategory(categoryRepository.findByName(request.getCategory()));
-        question.setType(typeRepository.findByName(request.getType()));
-        question.setDifficulty(difficultyRepository.findByName(request.getDifficulty()));
-        question.setContent(request.getContent());
 
-        // Gán ảnh từ Supabase (nếu có)
-        if (request.getImage() != null && !request.getImage().isBlank()) {
-            question.setImage(request.getImage());
-        }
+        resolveAndSetBasicFields(question, request);
 
-        // Map AnswerRequest → Answer
-        List<Answer> answerEntities = request.getAnswers().stream()
-                .map(dto -> {
-                    Answer answer = new Answer();
-                    answer.setContent(dto.getContent());
-                    answer.setCorrect(dto.isCorrect());
-                    answer.setQuestion(question); // quan hệ bidirectional
-                    return answer;
-                })
-                .collect(Collectors.toList());
+        List<Answer> answers = mapAnswers(request.getAnswers(), question);
+        question.setAnswers(answers);
 
-        question.setAnswers(answerEntities);
-
-        // Lưu vào DB
         questionRepository.save(question);
     }
 
@@ -237,71 +165,16 @@ public class QuestionService {
     }
 
     @Transactional
-    public void update(EditQuestionRequest request, long id, MultipartFile image) throws IOException {
+    public void updateQuestion(QuestionRequest request, long id) {
         Question question = findById(id);
-        Category category = categoryRepository.findByName(request.getCategory());
-        if (category == null) {
-            throw new NotFoundException(messageHelper.get("category.not.found"));
-        }
-        Type type = typeRepository.findByName(request.getType());
-        if (type == null) {
-            throw new NotFoundException(messageHelper.get("type.not.found"));
-        }
-        Difficulty difficulty = difficultyRepository.findByName(request.getDifficulty());
-        if (difficulty == null) {
-            throw new NotFoundException(messageHelper.get("difficulty.not.found"));
-        }
 
-        answerRepository.deleteAllByQuestionId(id);
+        resolveAndSetBasicFields(question, request);
 
-        question.setCategory(category);
-        question.setType(type);
-        question.setDifficulty(difficulty);
-        question.setContent(request.getContent());
+        question.getAnswers().clear(); // Xoá các câu trả lời cũ (nếu dùng orphanRemoval)
 
-        question.getAnswers().clear();
-        for (Answer item : request.getAnswers()) {
-            item.setQuestion(question);
-            question.getAnswers().add(item);
-        }
+        List<Answer> answers = mapAnswers(request.getAnswers(), question);
+        question.getAnswers().addAll(answers);
 
-        if (image == null) {
-            if (question.getImage() != null) {
-                try {
-                    String oldImage = question.getImage().replace(urlPrefix, "").replaceFirst("^/", "");
-                    Path oldPath = Paths.get(uploadDirectory, oldImage);
-                    Files.deleteIfExists(oldPath);
-                } catch (IOException e) {
-                    throw new UploadException("Xoá ảnh cũ thất bại: " + e.getMessage());
-                }
-            }
-            question.setImage(null);
-        } else if (!image.isEmpty()) {
-            try {
-                String originalName = StringUtils.cleanPath(Objects.requireNonNull(image.getOriginalFilename()));
-                if (originalName.isBlank()) {
-                    throw new UploadException(messageHelper.get("file.name.invalid"));
-                }
-
-                String contentType = image.getContentType();
-                if (contentType == null || !contentType.startsWith("image/")) {
-                    throw new UploadException(messageHelper.get("image.type.invalid"));
-                }
-                String extension = originalName.substring(originalName.lastIndexOf("."));
-                String baseName = originalName.substring(0, originalName.lastIndexOf(".")).replaceAll("[^a-zA-Z0-9_-]", "");
-                String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
-                String safeFileName = baseName + "_" + timestamp + extension;
-
-                Path uploadPath = Paths.get(uploadDirectory);
-                Files.createDirectories(uploadPath);
-
-                Path destination = uploadPath.resolve(safeFileName);
-                Files.copy(image.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
-                question.setImage(urlPrefix + safeFileName);
-            } catch (IOException e) {
-                throw new UploadException(messageHelper.get("file.upload.error") + ": " + e.getMessage());
-            }
-        }
         questionRepository.save(question);
     }
 
@@ -339,4 +212,38 @@ public class QuestionService {
             throw new LockedException(messageHelper.get("question.update.conflict"));
         }
     }
+
+    private void resolveAndSetBasicFields(Question question, QuestionRequest request) {
+        Category category = categoryRepository.findByName(request.getCategory());
+        if (category == null) throw new NotFoundException(messageHelper.get("category.not.found"));
+        question.setCategory(category);
+
+        Type type = typeRepository.findByName(request.getType());
+        if (type == null) throw new NotFoundException(messageHelper.get("type.not.found"));
+        question.setType(type);
+
+        Difficulty difficulty = difficultyRepository.findByName(request.getDifficulty());
+        if (difficulty == null) throw new NotFoundException(messageHelper.get("difficulty.not.found"));
+        question.setDifficulty(difficulty);
+
+        question.setContent(request.getContent());
+
+        if (request.getImage() != null && !request.getImage().isBlank()) {
+            question.setImage(request.getImage());
+        }
+    }
+
+    private List<Answer> mapAnswers(List<AnswerRequest> answerRequests, Question question) {
+        return answerRequests.stream()
+                .map(dto -> {
+                    Answer answer = new Answer();
+                    answer.setContent(dto.getContent());
+                    answer.setCorrect(dto.isCorrect());
+                    answer.setQuestion(question);
+                    return answer;
+                })
+                .collect(Collectors.toList());
+    }
+
+
 }
